3517ed745ad660652f0af9873b43d8f8f9b90ece
JuliaCarew <crewjulia3@gmail.com>
2024-12-02 15:13:28 -0400

    got more done on 2nd event, organized timeline event script/heirarchy

--- Assets/Scripts/Timeline Triggers.cs
+++ Assets/Scripts/Timeline Triggers.cs
@@ -6,14 +6,26 @@ using UnityEngine.Playables;
 public class TimelineTriggers : MonoBehaviour
 {
     public PlayableDirector timeline01;
+    public PlayableDirector timeline02;
+    public PlayableDirector timeline03;
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (gameObject.CompareTag("Event1"))
-        //{
+        if (gameObject.CompareTag("Event1"))
+        {
             Debug.Log("Triggered Event 1");
             timeline01.Play();
-        //}
+        }
+        if (gameObject.CompareTag("Event2"))
+        {
+            Debug.Log("Triggered Event 2");
+            timeline02.Play();
+        }
+        if (gameObject.CompareTag("Event3"))
+        {
+            Debug.Log("Triggered Event 3");
+            timeline03.Play();
+        }
     }
 }
 // event trigger scripts must be on the object that the player triggers in the scene (a.k.a. the green cube)
